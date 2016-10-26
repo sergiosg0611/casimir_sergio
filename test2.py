970ab6b3e9256b272795708918b2881b721a9ca8
sergio <sergio.m6@gmail.com>
2016-10-26 11:42:26 +0000

    another change to the file

--- test2.py
+++ test2.py
@@ -1,8 +1,8 @@
 area=0
 import numpy as np
 
-b=10
-h=2
+b=3.6
+h=2.2
 r=1
 
 area = 0.5*b*h
@@ -11,3 +11,4 @@ area_circle = pi*r**2
 print(area) # Print area of the triangle
 print(area_circle) # Print area of the circle
  
+# the input values of the file have changed 
