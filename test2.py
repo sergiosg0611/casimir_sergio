5835e04af4ac68ec87f8f1c06a9a303e9152be9c
sergio <sergio.m6@gmail.com>
2016-10-26 11:32:26 +0000

    First modification of the file by Sergio

--- test2.py
+++ test2.py
@@ -3,7 +3,11 @@ import numpy as np
 
 b=10
 h=2
+r=1
 
 area = 0.5*b*h
+area_circle = pi*r**2 
 
-print(area)
+print(area) # Print area of the triangle
+print(area_circle) # Print area of the circle
+ 
