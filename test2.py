fcc0433a09be41bf9965aa2fd2b5b5270789a783
Durgesh Kawale <durgeshkawale@gmail.com>
2016-10-26 12:21:05 +0000

    nur

--- test2.py
+++ test2.py
@@ -1,15 +1,13 @@
-<<<<<<< HEAD
-'''testing comments to describe the file ''''
+
+#'''testing comments to describe the file ''''
 area=100 # this is the initial area
 area=1
 area=0
 import numpy as np
 import matplotlib.pyplot as plt
-=======
+
 #### Comments on the file for future changes###
 
-area=100 # this is the initial area area=1 area=0 import numpy as np import matplotlib.pyplot as plt
->>>>>>> bc9263c2a6f14414c928066a1a9deff83a964b32
 
 b=3.6
 h=2.2
@@ -25,6 +23,7 @@ x = np.linspace(0, 2 *np.pi,101)
 y = np.sin(x)
 
 plt.plot(x,y)
+savefig('casimir.png')
 
  
 # the input values of the file have changed 
