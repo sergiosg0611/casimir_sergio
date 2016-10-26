9f5849a1739465315f834fb9f07a974ad2618255
sergio <sergio.m6@gmail.com>
2016-10-26 11:53:30 +0000

    Plot is inlcuded

--- test2.py
+++ test2.py
@@ -1,15 +1,21 @@
 area=0
 import numpy as np
+import matplotlib.pyplot as plt
 
 b=3.6
 h=2.2
 r=1
 
 area = 0.5*b*h
-area_circle = pi*r**2 
+area_circle = np.pi*r**2 
 
 print(area) # Print area of the triangle
 print(area_circle) # Print area of the circle
+
+x = np.linspace(0, 2 *np.pi,101)
+y = np.sin(x)
+
+plt.plot(x,y)
 
  
 # the input values of the file have changed 
